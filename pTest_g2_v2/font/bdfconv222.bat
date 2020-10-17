27b066c5ed2d98338020564b96d5e088b51db446
74ls00 <userled@yandex.ru>
2020-10-18 01:46:12 +0300

    testv2

---
+++ pTest_g2_v2/font/bdfconv222.bat
@@ -0,0 +1,28 @@
+rem example
+rem bdfconv_2_22.exe -v -f 1 -m "32-127" ../bdf/helvB18.bdf -o helvb18_tf.c  -n u8g2_font_helvB18_tf -d ../bdf/helvB18.bdf
+rem type helvb18_tf.c
+cls
+
+set "bdfconv_2_22=G:\home\Documents\Projects\0-Electronics\E-bike\0-Display\u8g2\tools\font\bdfconv\bdfconv_2_22.exe"
+
+set font_name=u8g2_font_font18dig
+
+set "file_c=G:\home\Documents\Projects\0-Electronics\E-bike\0-Display\LCD12864_ST7920_u8g2\pTest_g2\font\%font_name%.c"
+
+set "font_bdf=G:\home\Documents\Projects\0-Electronics\E-bike\0-Display\LCD12864_ST7920_u8g2\pTest_g2\font\ncenB18m.bdf"
+
+
+del "%file_c%"
+
+
+
+%bdfconv_2_22% -v -f 1 -m "46-102" %font_bdf% -o %file_c%  -n %font_name%
+
+
+
+rem pause
+
+rem set "file_c=G:\home\Documents\Projects\0-Electronics\E-bike\0-Display\LCD12864_ST7920_u8g2\pTest_g2\font\helvb18_tf.c"
+
+
+rem bdfconv_2_22.exe -v -f 1 -m "32-127" ../bdf/helvB18.bdf -o helvb18_tf.c  -n u8g2_font_helvB18_tf -d ../bdf/helvB18.bdf
